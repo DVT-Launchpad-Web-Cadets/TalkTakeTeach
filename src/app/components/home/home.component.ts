b44cdc3ecbfba6f9bdd401ec1a6139857449db2d
juliannaventer <juliannaventer@gmail.com>
2024-06-20 14:00:13 +0200

    feat: :sparkles: create search and products-results components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ProductResultsComponent } from './product-results/product-results.component';
+import { SearchComponent } from './search/search.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductResultsComponent, SearchComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
---
+++ src/app/components/home/product-results/product-results.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-results',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-results.component.html',
+  styleUrl: './product-results.component.scss'
+})
+export class ProductResultsComponent {
+
+}
