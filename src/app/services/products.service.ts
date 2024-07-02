453d96a14767806239ef370e69e2ba787f5e2a01
tanadendere <tanadendere@gmail.com>
2024-07-02 14:25:37 +0200

    feat: :sparkles: Changed search url to connect to backend search endpoint

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,17 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import ProductInterface from '../interfaces/products';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  private baseUrl = 'assets/products.json';
+  private searchUrl = environment.baseUrl + environment.searchSuffix;
 
   constructor(private http: HttpClient) {}
 
   getProducts(searchValue: string): Observable<ProductInterface[]> {
-    return this.http.get<ProductInterface[]>(this.baseUrl);
+    return this.http.get<ProductInterface[]>(this.searchUrl + searchValue);
     //actual call that will be made:
     // return this.http.get<ProductInterface[]>(`${this.baseUrl}?search=${searchValue}`);
   }
